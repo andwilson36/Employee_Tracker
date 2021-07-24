2541143f47abc912bcfdc1d612957e14c9d9a806
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-23 19:50:40 -0500

    made server

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const express = require('express');
 const mysql = require('mysql2');
-const inquirer = require('inquirer');
+// PORT
+const PORT = process.env.PORT || 3001;
+// middleware
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'classlist_db'
+    },
+    console.info('Connected to database successfully.')
+);
+
+db.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+});
+
+// default response
+app.use((req, res => {
+    res.status(404).end();
+}));
+
+app.listen(PORT, () => {
+    console.info(`Server listening at ${PORT}`);
+})
