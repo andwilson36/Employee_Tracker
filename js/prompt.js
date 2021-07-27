2e092f2f49af324cd552961c9c475a377f942d29
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-27 16:22:03 -0500

    config file, redone prompt, .env added for security

--- js/prompt.js
+++ js/prompt.js
@@ -1,40 +1,6 @@
-const inquirer = require('inquirer');
-
-function Prompt() {}
-
-var choice;
-
-Prompt.prototype.menuPrompt = function() {
-    inquirer
-        .prompt([{
-            type: 'list',
-            name: 'menu',
-            message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
-        }])
-        .then((response) => {
-            const result = response.menu;
-            switch (result) {
-                case 'View All Employees':
-                    return 'View All Employees';
-                case 'Add Employee':
-                    return 'Add Employee';
-                case 'Update Employee Role':
-                    return 'Update Employee Role';
-                case 'View All Roles':
-                    return 'View All Roles';
-                case 'Add Role':
-                    return 'Add Role';
-                case 'View All Departments':
-                    return 'View All Departments';
-                case 'Add Department':
-                    return 'Add Department';
-                case 'Quit':
-                    choice = 'Quit';
-                    return 'Quit';
-            }
-        })
-}
-
-module.exports = { choice };
-module.exports = Prompt;
+const menuQuestions = [{
+    type: 'list',
+    name: 'menu',
+    message: 'What would you like to do?',
+    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+}]
--- server.js
+++ server.js
@@ -1,40 +1,16 @@
 const express = require('express');
 const mysql = require('mysql2');
-const Prompt = require('./js/prompt');
+const sequelize = require('./config/connection');
 const Logo = require('./js/logo');
 const logo = new Logo();
-const displayPrompt = new Prompt();
-import choice from './js/prompt.js';
 // PORT
 const PORT = process.env.PORT || 3001;
 // middleware
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Connect to database
-const db = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'employee_db'
-    },
-    logo.displayLogo(),
-    console.info('Connected to database successfully.')
-);
-
-// db.query('SELECT * FROM department', function (err, results) {
-//     console.log(results);
-// });
-
-// // default response
-// app.use((req, res) => {
-//     res.status(404).end();
-// });
-
-// app.listen(PORT, () => {
-//     console.info(`Server listening at ${PORT}`);
-// });
-// to display the prompt
-displayPrompt.menuPrompt();
-console.info(choice);
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+    logo.displayLogo()
+});
