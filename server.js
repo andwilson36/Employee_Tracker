a38a0d7229a34232ec3983f8c7adfabc423cbd1e
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-27 20:56:09 -0500

    added if statement for view employees

--- server.js
+++ server.js
@@ -23,44 +23,15 @@ function prompt() {
                         .then((response) => {
                             switch (response.menu) {
                                 case 'View All Employees':
-                                    view.employees();
+                                    view.employees(response.menu);
                                     break;
                             }
                         })
-                        //     }
-                        //     case 'Add Employee':
-                        //         console.log('Add Employee');
-                        //         break;
-                        //         return 'Add Employee';
-                        //     case 'Update Employee Role':
-                        //         console.log('Update Employee Role');
-                        //         break;
-                        //         return 'Update Employee Role';
-                        //     case 'View All Roles':
-                        //         console.log('View All Roles');
-                        //         break;
-                        //         return 'View All Roles';
-                        //     case 'Add Role':
-                        //         console.log('Add Role');
-                        //         break;
-                        //         return 'Add Role';
-                        //     case 'View All Departments':
-                        //         console.log('View All Departments');
-                        //         break;
-                        //         return 'View All Departments';
-                        //     case 'Add Department':
-                        //         console.log('Add Department');
-                        //         break;
-                        //         return 'Add Department';
-                        //     case 'Quit':
-                        //         console.log('Quit');
-                        //         break;
-                        //         choice = 'Quit';
-                        //         return 'Quit';
-                        // })
-            };
-        });
-};
+                    break;
+                case 'Add Employee':
+            }
+        })
+}
 
 logo.displayLogo()
 
