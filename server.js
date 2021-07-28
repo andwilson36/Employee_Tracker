99e20d1ce594d2f62e9ca7728033aae1dc5f3139
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-28 18:49:01 -0500

    added npm start, view all employees, add employee

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const inquirer = require('inquirer');
-const prompter = require('./lib/prompt');
-const ViewAll = require('./lib/tableData');
-const view = new ViewAll();
 const Logo = require('./lib/logo');
 const logo = new Logo();
-
-// PORT
+const prompt = require('./lib/prompt')
+    // PORT
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,27 +10,5 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-function prompt() {
-    inquirer.prompt(prompter.menuQuestions)
-        .then((response) => {
-            switch (response.menu) {
-                case 'View Employees':
-                    inquirer.prompt(prompter.menuViewEmployees)
-                        .then((response) => {
-                            switch (response.menu) {
-                                case 'View All Employees':
-                                    view.employees(response.menu);
-                                    break;
-                            }
-                        })
-                    break;
-                case 'Add Employee':
-            }
-        })
-}
-
-logo.displayLogo()
-
-app.listen(PORT, () => {
-    prompt();
-});
+logo.displayLogo();
+prompt.menuQuestions();
